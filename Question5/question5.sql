97c4675bd2c7691078b6a91e4852e0ab3061380c
kongphiphobz <kongphiphobz@gmail.com>
2022-04-14 16:11:01 +0700

    edit

--- Question5/question5.sql
+++ Question5/question5.sql
@@ -1,7 +1,7 @@
-SELECT users.*, users.age, zipcodes.zipcode, district.name, province.name, subdistrict.name 
+SELECT users.*, user.age, zipcodes.zipcode, district.name, province.name, subdistrict.name 
 FROM users
 JOIN zipcode ON users.zipcode_fk = zipcodes.zipcode
 JOIN district ON zipcodes.district_fk = district.id
 JOIN provinces ON zipcodes.provinces_fk = provinces.id
 JOIN subdistrict ON zipcodes.subdistrict_fk = subdistrict.id
-WHERE users.username
+WHERE users.username = 'paiduay' OR users.username = 'kasidit'
